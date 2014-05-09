fcb3a42b2e2df1b6c8be702ef1d14fdee87e603f
Hao Song <hao.song@hp.com>
2014-05-09 10:37:30 +0800

    kernel: add log level

---
+++ kernel/log/loglevel.c
@@ -0,0 +1,9 @@
+#defineKERN_EMERG"<0>"
+#defineKERN_ALERT"<1>"
+#defineKERN_CRIT"<2>"
+#defineKERN_ERR"<3>"
+#defineKERN_WARNING"<4>"
+#defineKERN_NOTICE"<5>"
+#defineKERN_INFO"<6>"
+#defineKERN_DEBUG"<7>"
+
